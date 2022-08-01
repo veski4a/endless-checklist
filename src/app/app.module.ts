9b785bdabfcb2fc0803befb1bee9a1179f023276
veski4a <veski4a92@gmail.com>
2022-08-01 14:15:38 +0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ButtonModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
