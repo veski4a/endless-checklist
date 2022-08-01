a0f85d5969b2e5eaba92f74445c7c5576df8bb39
veski4a <veski4a92@gmail.com>
2022-08-01 15:07:38 +0300

    5 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ChecklistComponent } from './checklist/checklist.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, CheckboxModule],
-  declarations: [AppComponent, HelloComponent, ChecklistComponent],
+  declarations: [AppComponent, ChecklistComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
