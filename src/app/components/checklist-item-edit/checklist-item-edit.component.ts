e45a5b7cbc4c4fe34b8e8b43bee50212f35ace87
veski4a <veski4a92@gmail.com>
2022-08-02 17:49:35 +0300

    5 files modified, 1 added

--- src/app/components/checklist-item-edit/checklist-item-edit.component.ts
+++ src/app/components/checklist-item-edit/checklist-item-edit.component.ts
@@ -5,27 +5,45 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   templateUrl: './checklist-item-edit.component.html',
   styleUrls: ['./checklist-item-edit.component.css'],
 })
+/**
+ * Component representing the checklist item in edit mode
+ */
 export class ChecklistItemEditComponent implements OnInit {
+  // Task's name
   public taskName: string = '';
-  public autoResize: boolean = true;
+  // Whether the task name input box should be auto resized based on the text's length
+  public taskNameInputAutoResize: boolean = true;
 
-  @Output() onAddNewTask = new EventEmitter<string>();
+  // Callback when the user clicks save
+  @Output() onSave = new EventEmitter<string>();
+  // Callback when the user cancels out of the edit
   @Output() onCancel = new EventEmitter();
 
   constructor() {}
 
-  ngOnInit() {}
-
-  public get isAddButtonDisabled(): boolean {
-    return this.taskName.length == 0;
+  ngOnInit() {
+    console.log('OnInit');
   }
 
-  public add(): void {
-    this.onAddNewTask.emit(this.taskName);
+  /**
+   * Called when the user clicks the save button
+   */
+  public save(): void {
+    this.onSave.emit(this.taskName);
     this.taskName = '';
   }
 
+  /**
+   * Called when the user clicks the cancel button
+   */
   public cancel(): void {
     this.onCancel.emit();
+  }
+
+  /**
+   * Getter controlling the save button's disabled property
+   */
+  public get isSaveDisabled(): boolean {
+    return this.taskName.length == 0;
   }
 }
