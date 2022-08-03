88879f86808ae86d2eeffc8cbb902a9ef05a1439
veski4a <veski4a92@gmail.com>
2022-08-03 05:27:11 +0300

    1 file modified

--- src/app/components/checklist-add-item/checklist-add-item.component.ts
+++ src/app/components/checklist-add-item/checklist-add-item.component.ts
@@ -5,6 +5,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   templateUrl: './checklist-add-item.component.html',
   styleUrls: ['./checklist-add-item.component.css'],
 })
+/**
+ * A component for adding a task in a checklist
+ */
 export class ChecklistAddItemComponent implements OnInit {
   // Callback when the user adds a task
   @Output() onAdd = new EventEmitter<string>();
@@ -13,6 +16,9 @@ export class ChecklistAddItemComponent implements OnInit {
 
   ngOnInit() {}
 
+  /**
+   * Called when the user adds a task
+   */
   addTask(taskName: string): void {
     this.onAdd.emit(taskName);
   }
